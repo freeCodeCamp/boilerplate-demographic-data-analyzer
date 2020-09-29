ecb71658a1c5afafa19ca7c3080f4c668443d7d9
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-29 12:23:25 -0500

    init

---
+++ demographic_data_analyzer.py
@@ -0,0 +1,69 @@
+import pandas as pd
+
+
+def calculate_demographic_data(print_data=True):
+    # Read data from file
+    df = None
+
+    # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
+    race_count = None
+
+    # What is the average age of men?
+    average_age_men = None
+
+    # What is the percentage of people who have a Bachelor's degree?
+    percentage_bachelors = None
+
+    # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
+    # What percentage of people without advanced education make more than 50K?
+
+    # with and without `Bachelors`, `Masters`, or `Doctorate`
+    higher_education = None
+    lower_education = None
+
+    # percentage with salary >50K
+    higher_education_rich = None
+    lower_education_rich = None
+
+    # What is the minimum number of hours a person works per week (hours-per-week feature)?
+    min_work_hours = None
+
+    # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
+    num_min_workers = None
+
+    rich_percentage = None
+
+    # What country has the highest percentage of people that earn >50K?
+    highest_earning_country = None
+    highest_earning_country_percentage = None
+
+    # Identify the most popular occupation for those who earn >50K in India.
+    top_IN_occupation = None
+
+    # DO NOT MODIFY BELOW THIS LINE
+
+    if print_data:
+        print("Number of each race:\n", race_count) 
+        print("Average age of men:", average_age_men)
+        print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
+        print(f"Percentage with higher education that earn >50K: {higher_education_rich}%")
+        print(f"Percentage without higher education that earn >50K: {lower_education_rich}%")
+        print(f"Min work time: {min_work_hours} hours/week")
+        print(f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
+        print("Country with highest percentage of rich:", highest_earning_country)
+        print(f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
+        print("Top occupations in India:", top_IN_occupation)
+
+    return {
+        'race_count': race_count,
+        'average_age_men': average_age_men,
+        'percentage_bachelors': percentage_bachelors,
+        'higher_education_rich': higher_education_rich,
+        'lower_education_rich': lower_education_rich,
+        'min_work_hours': min_work_hours,
+        'rich_percentage': rich_percentage,
+        'highest_earning_country': highest_earning_country,
+        'highest_earning_country_percentage':
+        highest_earning_country_percentage,
+        'top_IN_occupation': top_IN_occupation
+    }
---
+++ main.py
@@ -0,0 +1,9 @@
+# This entrypoint file to be used in development. Start by reading README.md
+import demographic_data_analyzer
+from unittest import main
+
+# Test your function by calling it here
+demographic_data_analyzer.calculate_demographic_data()
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
